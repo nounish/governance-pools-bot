97f88e3e9d75af5800c0125c25e750f777846a62
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2023-12-03 18:13:47 -0600

    end to end testnet for l2 governance

--- index.test.ts
+++ index.test.ts
@@ -1,10 +1,8 @@
 import BotSwarm from "@federationwtf/botswarm";
-import {
-  FederationNounsPool,
-  FederationNounsGovernor,
-  FederationNounsRelayer,
-} from "@federationwtf/botswarm/contracts";
-import { RelicClient } from "@relicprotocol/client";
+import { FederationNounsPool } from "@federationwtf/botswarm/contracts";
+import MockNounsGovernor from "./contracts/MockNounsGovernor";
+import MockNounsRelayer from "./contracts/MockNounsRelayer";
+import Relic from "@relicprotocol/client";
 import { ethers } from "ethers";
 
 const { Ethereum } = BotSwarm();
@@ -13,22 +11,29 @@ const ethersProvider = new ethers.providers.JsonRpcProvider(
   process.env.SEPOLIA_RPC_URL as string
 );
 
-const relic = await RelicClient.fromProvider(ethersProvider);
+const relic = await Relic.RelicClient.fromProvider(ethersProvider);
 
 const { addTask, tasks, rescheduleTask, watch, read, clients } = Ethereum({
   contracts: {
     FederationNounsPool,
-    FederationNounsGovernor,
-    FederationNounsRelayer,
+    MockNounsGovernor,
+    MockNounsRelayer,
   },
   hooks: {
-    getBlockProof: async (task, block) => {
-      const { blockHash } = await clients.sepolia.getTransaction({
-        blockNumber: block,
+    getBlockProof: async (task) => {
+      const { startBlock } = await read({
+        contract: "MockNounsGovernor",
+        chain: "sepolia",
+        functionName: "getProposal",
+        args: [task.args[0]],
+      });
+
+      const { hash } = await clients.sepolia.getTransaction({
+        blockNumber: startBlock + 2n,
         index: 0,
       });
 
-      const receipt = await ethersProvider.getTransactionReceipt(blockHash);
+      const receipt = await ethersProvider.getTransactionReceipt(hash);
 
       const { proof } = await relic.transactionProver.getProofData(receipt);
 
@@ -38,6 +43,7 @@ const { addTask, tasks, rescheduleTask, watch, read, clients } = Ethereum({
     },
   },
   privateKey: process.env.ETHEREUM_PRIVATE_KEY as string,
+  cacheTasks: false,
 });
 
 // Governance Pools
@@ -74,29 +80,29 @@ watch(
 // Governor
 watch(
   {
-    contract: "FederationNounsGovernor",
-    chain: "zkSyncTestnet",
+    contract: "MockNounsGovernor",
+    chain: "sepolia",
     event: "VoteCast",
   },
   async (event) => {
-    const [, , , , , , , , , , , castWindow, finalityBlocks] = await read({
-      contract: "FederationNounsGovernor",
-      chain: "zkSyncTestnet",
-      functionName: "config",
-    });
+    // const [, , , , , , , , , , , castWindow, finalityBlocks] = await read({
+    //   contract: "MockNounsGovernor",
+    //   chain: "sepolia",
+    //   functionName: "config",
+    // });
 
-    const { endBlock } = await read({
-      contract: "FederationNounsGovernor",
-      chain: "zkSyncTestnet",
-      functionName: "getProposal",
-      args: [event.args.proposal],
-    });
+    // const { endBlock } = await read({
+    //   contract: "MockNounsGovernor",
+    //   chain: "sepolia",
+    //   functionName: "getProposal",
+    //   args: [event.args.proposal],
+    // });
 
     addTask({
-      block: endBlock - (castWindow + finalityBlocks),
+      block: event.blockNumber + 2n,
       hooks: ["getBlockProof"],
-      contract: "FederationNounsGovernor",
-      chain: "zkSyncTestnet",
+      contract: "MockNounsGovernor",
+      chain: "sepolia",
       functionName: "settleVotes",
       // @ts-ignore
       args: [event.args.proposal],
@@ -107,18 +113,18 @@ watch(
 // Relayer
 watch(
   {
-    contract: "FederationNounsGovernor",
-    chain: "zkSyncTestnet",
+    contract: "MockNounsGovernor",
+    chain: "sepolia",
     event: "VotesSettled",
   },
   async (event) => {
     addTask({
-      block: event.blockNumber,
-      hooks: ["getMessageProof"],
-      contract: "FederationNounsRelayer",
+      block: event.blockNumber + 2n,
+      // hooks: ["getMessageProof"],
+      contract: "MockNounsRelayer",
       chain: "sepolia",
       functionName: "relayVotes",
-      args: [event.args.proposal],
+      args: [event.args],
     });
   }
 );
--- index.ts
+++ index.ts
@@ -1,11 +1,41 @@
 import BotSwarm from "@federationwtf/botswarm";
-import { FederationNounsPool } from "@federationwtf/botswarm/contracts";
+import { FederationNounsPool, FederationNounsGovernor, FederationNounsRelayer } from "@federationwtf/botswarm/contracts";
+import Relic from "@relicprotocol/client";
+import { ethers } from "ethers";
 
 const { Ethereum } = BotSwarm();
 
-const { addTask, tasks, rescheduleTask, watch, read } = Ethereum({
+const ethersProvider = new ethers.providers.JsonRpcProvider(
+  process.env.MAINNET_RPC_URL as string
+);
+
+const relic = await Relic.RelicClient.fromProvider(ethersProvider);
+
+const { addTask, tasks, rescheduleTask, watch, read, clients } = Ethereum({
   contracts: {
     FederationNounsPool,
+    FederationNounsGovernor,
+    FederationNounsRelayer,
+  },
+  hooks: {
+    getBlockProof: async (task, block) => {
+      const { hash } = await clients.sepolia.getTransaction({
+        blockNumber: block - 5n,
+        index: 0,
+      });
+
+      const receipt = await ethersProvider.getTransactionReceipt(hash);
+
+      const { proof } = await relic.transactionProver.getProofData(receipt);
+
+      task.args.push(proof);
+
+      return task;
+    },
+    getMessageProof: async (task, block) => {
+      // TOOD: get message proof
+      return task;
+    },
   },
   privateKey: process.env.ETHEREUM_PRIVATE_KEY as string,
 });
@@ -38,3 +68,65 @@ watch(
     }
   }
 );
+
+// L2 Governance
+
+// Governor
+watch(
+  {
+    contract: "FederationNounsGovernor",
+    chain: "zkSync",
+    event: "VoteCast",
+  },
+  async (event) => {
+    const [, , , , , , , , , , , castWindow, finalityBlocks] = await read({
+      contract: "FederationNounsGovernor",
+      chain: "zkSync",
+      functionName: "config",
+    });
+
+    const { endBlock } = await read({
+      contract: "FederationNounsGovernor",
+      chain: "zkSync",
+      functionName: "getProposal",
+      args: [event.args.proposal],
+    });
+
+    addTask({
+      block: endBlock - (castWindow + finalityBlocks) + 10n,
+      hooks: ["getBlockProof"],
+      contract: "FederationNounsGovernor",
+      chain: "zkSync",
+      functionName: "settleVotes",
+      // @ts-ignore
+      args: [event.args.proposal],
+    });
+  }
+);
+
+// Relayer
+watch(
+  {
+    contract: "FederationNounsGovernor",
+    chain: "zkSync",
+    event: "VotesSettled",
+  },
+  async (event) => {
+    const [, , , , , , , , , , , , finalityBlocks] = await read({
+      contract: "FederationNounsGovernor",
+      chain: "zkSync",
+      functionName: "config",
+    });
+    
+    addTask({
+      block: event.blockNumber + finalityBlocks,
+      hooks: ["getMessageProof"],
+      contract: "FederationNounsRelayer",
+      chain: "mainnet",
+      functionName: "relayVotes",
+      // TODO: add message proof stuff
+      args: [event.args.proposal],
+    });
+  }
+);
+
