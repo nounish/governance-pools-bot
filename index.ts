7bfae5b6e5d7f3ee26ce825bcdcae06c8df43356
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-01-07 15:01:27 -0600

    finished support for l2 governance (hopefull)

--- index.ts
+++ index.ts
@@ -1,29 +1,50 @@
 import BotSwarm from "@federationwtf/botswarm";
-import { FederationNounsPool, FederationNounsGovernor, FederationNounsRelayer } from "@federationwtf/botswarm/contracts";
+import {
+  FederationNounsPool,
+  FederationNounsGovernor,
+  FederationNounsRelayer,
+  NounsDAOLogicV3,
+} from "@federationwtf/botswarm/contracts";
 import Relic from "@relicprotocol/client";
 import { ethers } from "ethers";
 import { Provider } from "zksync-web3";
 
-const { Ethereum } = BotSwarm();
+const { Ethereum, log } = BotSwarm();
 
 const mainnetProvider = new ethers.providers.JsonRpcProvider(
-  process.env.MAINNET_RPC_URL as string
+  "https://rpc.flashbots.net/"
 );
+const mainnetWallet = new ethers.Wallet(
+  process.env.ETHEREUM_PRIVATE_KEY as string
+);
+const mainnetSigner = mainnetWallet.connect(mainnetProvider);
 
 const relic = await Relic.RelicClient.fromProvider(mainnetProvider);
 
 const zkSyncProvider = new Provider(process.env.ZKSYNC_RPC_URL as string);
 
-const { addTask, tasks, rescheduleTask, watch, read, clients, contracts } = Ethereum({
+const {
+  addTask,
+  tasks,
+  rescheduleTask,
+  watch,
+  read,
+  clients,
+  contracts,
+  schedule,
+} = Ethereum({
   contracts: {
     FederationNounsPool,
     FederationNounsGovernor,
     FederationNounsRelayer,
+    NounsDAOLogicV3,
   },
   hooks: {
-    getBlockProof: async (task, block) => {
-      const { hash } = await clients.sepolia.getTransaction({
-        blockNumber: block - 5n,
+    getBlockProof: async (task) => {
+      log.active(`Getting block proof for proposal end block ${task.args[1]}`);
+
+      const { hash } = await clients.mainnet.getTransaction({
+        blockNumber: task.args[1],
         index: 0,
       });
 
@@ -31,9 +52,7 @@ const { addTask, tasks, rescheduleTask, watch, read, clients, contracts } = Ethe
 
       const { proof } = await relic.transactionProver.getProofData(receipt);
 
-      task.args.push(proof);
-
-      return task;
+      return { ...task, args: [task.args[0], proof] };
     },
     getMessageProof: async (task) => {
       const messageHash = ethers.utils.keccak256(task.args[3]);
@@ -48,7 +67,52 @@ const { addTask, tasks, rescheduleTask, watch, read, clients, contracts } = Ethe
         throw new Error("No proof found");
       }
 
-      return {...task, args: [task.args[0], proofInfo.id, task.args[2], task.args[3], proofInfo.proof]};
+      return {
+        ...task,
+        args: [
+          task.args[0],
+          proofInfo.id,
+          task.args[2],
+          task.args[3],
+          proofInfo.proof,
+        ],
+      };
+    },
+    publishBlockHash: async (task) => {
+      log.active(`Publishing block hash to Relic for block ${task.args[1]}`);
+
+      const relic = await Relic.RelicClient.fromProvider(mainnetProvider);
+
+      // make sure theres no reorg
+      const blockHash = await mainnetProvider
+        .getBlock(Number(task.args[1]))
+        .then((b) => b.hash);
+
+      await mainnetSigner.sendTransaction(
+        await relic.bridge.sendBlock(blockHash)
+        // {} // optional gas configuration goes here
+      );
+
+      await relic.bridge.waitUntilBridged(blockHash);
+
+      return task;
+    },
+  },
+  scripts: {
+    publishBlockHash: async (block: number) => {
+      const relic = await Relic.RelicClient.fromProvider(mainnetProvider);
+
+      // make sure theres no reorg
+      const blockHash = await mainnetProvider
+        .getBlock(block)
+        .then((b) => b.hash);
+
+      await mainnetSigner.sendTransaction(
+        await relic.bridge.sendBlock(blockHash)
+        // {} // optional gas configuration goes here
+      );
+
+      await relic.bridge.waitUntilBridged(blockHash);
     },
   },
   privateKey: process.env.ETHEREUM_PRIVATE_KEY as string,
@@ -85,6 +149,22 @@ watch(
 
 // L2 Governance
 
+// Publish proposal startBlock hash to Relic
+watch(
+  {
+    contract: "NounsDAOLogicV3",
+    chain: "mainnet",
+    event: "ProposalCreated",
+  },
+  async (event) => {
+    schedule({
+      name: "publishBlockHash",
+      block: Number(event.args.startBlock) + 1,
+      args: [Number(event.args.startBlock)],
+    });
+  }
+);
+
 // Governor
 watch(
   {
@@ -107,13 +187,13 @@ watch(
     });
 
     addTask({
-      block: endBlock - (castWindow + finalityBlocks) + 10n,
+      block: endBlock - (castWindow + finalityBlocks) + 1n,
       hooks: ["getBlockProof"],
       contract: "FederationNounsGovernor",
       chain: "zkSync",
       functionName: "settleVotes",
       // @ts-ignore
-      args: [event.args.proposal],
+      args: [event.args.proposal, endBlock],
     });
   }
 );
@@ -132,16 +212,22 @@ watch(
       functionName: "config",
     });
 
-    const {l1BatchNumber, l1BatchTxIndex, blockNumber} = await zkSyncProvider.getTransactionReceipt(event.transactionHash);
+    const { l1BatchNumber, l1BatchTxIndex, blockNumber } =
+      await zkSyncProvider.getTransactionReceipt(event.transactionHash);
 
     const encodedMessage = ethers.utils.AbiCoder.prototype.encode(
       ["uint256", "uint256", "uint256", "uint256"],
-      [Number(event.args.proposal), Number(event.args.forVotes), Number(event.args.againstVotes), Number(event.args.abstainVotes)]
+      [
+        Number(event.args.proposal),
+        Number(event.args.forVotes),
+        Number(event.args.againstVotes),
+        Number(event.args.abstainVotes),
+      ]
     ) as `0x${string}`;
 
     const proof = {
       id: 0n, // Overriden by hook
-      proof: ["0xPROOF"],  // Overriden by hook
+      proof: ["0xPROOF"], // Overriden by hook
     } as const;
 
     addTask({
@@ -150,11 +236,15 @@ watch(
       contract: "FederationNounsRelayer",
       chain: "mainnet",
       functionName: "relayVotes",
-      args: [BigInt(l1BatchNumber), proof.id, l1BatchTxIndex, encodedMessage, proof.proof, 
+      args: [
+        BigInt(l1BatchNumber),
+        proof.id,
+        l1BatchTxIndex,
+        encodedMessage,
+        proof.proof,
         //@ts-ignore
-        blockNumber
+        blockNumber,
       ],
     });
   }
 );
-
